8bf9c3845a60ddc7b919ed7d2e707eb03b4129f0
VSprudius <aaaaafffff902@gmail.com>
2024-08-08 14:19:35 +0500

    добавил подсчет мячиков которые находятся на форме

---
+++ GameBalls/CreateBalls.cs
@@ -0,0 +1,95 @@
+﻿using System.Data;
+using System.Windows.Forms;
+using Timer = System.Windows.Forms.Timer;
+
+namespace GameBalls
+{
+    public class CreateBalls
+    {
+        protected Form1 form1;
+        protected int x;
+        protected int y;
+        protected int width = 70;
+        protected int height = 70;
+        static Random random = new Random();
+        public CreateBalls(Form1 form)
+        {
+            form1 = form;
+        }
+        public void Show()
+        {
+            var graph = form1.CreateGraphics();
+            var brush = Brushes.Aqua;
+            var rectangle = new Rectangle(x, y, width, height);
+            graph.FillEllipse(brush, rectangle);
+        }
+
+        public void Clear()
+        {
+            var graph = form1.CreateGraphics();
+            var brush = Brushes.White;
+            var rectangle = new Rectangle(x, y, width, height);
+            graph.FillEllipse(brush, rectangle);
+        }
+
+        public void Go()
+        {
+            x += random.Next(-30,30);
+            y += random.Next(-15,15);
+        }
+        public void Move()
+        {
+            Clear();
+            Go();
+            Show();
+        }
+        public int ShowX()
+        {
+            return x;
+        }
+        public int ShowY()
+        {
+            return y;
+        }
+
+    }
+    
+    public class RandomBalls : CreateBalls
+    {
+        static Random random = new Random();
+        public RandomBalls(Form1 form) : base(form)
+        {
+            x = random.Next(0, form.ClientSize.Width - width);
+            y = random.Next(0, form.ClientSize.Height - height);
+        }
+    }
+    
+    public class MoveBall : RandomBalls
+    {
+        private Timer timer;
+        public MoveBall(Form1 form) : base(form)
+        {
+            timer = new Timer();
+            timer.Interval = 2;
+            timer.Tick += Timer_Tick;
+        }
+
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            Move();
+        }
+
+        public void Start()
+        {
+            timer.Start();
+        }
+        public void Stop()
+        {
+            timer.Stop();
+        }
+        public bool FindBalls()
+        {
+           return x>= 0  && x + width <= form1.ClientSize.Width && y>=0 && y + height <= form1.ClientSize.Height;
+        }
+    }
+}
--- GameBalls/Form1.cs
+++ GameBalls/Form1.cs
@@ -1,10 +1,49 @@
+using System.Runtime.CompilerServices;
+
 namespace GameBalls
 {
     public partial class Form1 : Form
     {
+        List<MoveBall> balls;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            BackColor = Color.White;
+            balls = new List<MoveBall>();
+            for (int i = 0; i < 10; i++)
+            {
+                var moveBall = new MoveBall(this);
+                balls.Add(moveBall);
+                balls[i].Start();
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var count = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                balls[i].Stop();
+                if (balls[i].FindBalls())
+                {
+                    count++;
+                }
+            }
+            MessageBox.Show($"Количество шариков которые удалось поймать: {count.ToString()}");
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                balls[i].Clear();
+            }
+            BackColor = Color.White;
         }
     }
 }
